2a45813860e3ee9eabe71a0e8c596a05172b66b9
pswaao88 <66404498+pswaao88@users.noreply.github.com>
2025-02-13 23:19:48 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 296 ms, Memory: 45020 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -1,52 +1,47 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int cnt = 0;
-	static StringBuilder sb = new StringBuilder();
-	public static void xToY(int n , int x, int y) {
-		//n은 x => y 1~n-1은 x => z로 이동
-		int z;
-		if((x == 1 && y == 2) || (x == 2 && y == 1)) {
-			z = 3;
-		}else if((x == 1 && y == 3) || (x == 3 && y == 1)) {
-			z = 2;
-		}else {
-			z = 1;
-		}
-		if(n == 1) {
-			sb.append(x);
-			sb.append(" ");
-			sb.append(y);
-			sb.append("\n");
-			cnt++;
-			return;
-		}else {
-			xToY(n-1, x, z);
-			sb.append(x);
-			sb.append(" ");
-			sb.append(y);
-			sb.append("\n");
-			cnt++;
-			xToY(n-1,z,y);
-		}
-		
-		
-		
-	}
-	public static void main(String[] args)throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int N = Integer.parseInt(br.readLine());
-		xToY(N,1,3);
-		bw.write(Integer.toString(cnt)+"\n");
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-		br.close();
-	}
+    static StringBuilder sb = new StringBuilder();
+    static int cnt = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        hanoi(N, 1,3);
+        bw.write(Integer.toString(cnt));
+        bw.newLine();
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+    static void hanoi(int N, int x, int y){// N을 x => y로이동
+        cnt++;
+        if(N == 1){
+            sb.append(x);
+            sb.append(" ");
+            sb.append(y);
+            sb.append("\n");
+            return;
+        }
+        int z = setZ(x,y);
+        hanoi(N-1, x, z);
+        sb.append(x);
+        sb.append(" ");
+        sb.append(y);
+        sb.append("\n");
+        hanoi(N-1, z, y);
+    }
+    static int setZ(int x, int y){
+        if((x == 1 && y == 2) || (x == 2 && y == 1)){
+            return 3;
+        }
+        if((x == 2 && y == 3) || (x == 3 && y == 2)){
+            return 1;
+        }
+        return 2;
+    }
 
 }
