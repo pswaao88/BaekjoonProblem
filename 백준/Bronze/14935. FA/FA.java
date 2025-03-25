5f5d9191dfe18b0ad5747f24bdc091e03987d1ed
pswaao88 <66404498+pswaao88@users.noreply.github.com>
2025-03-25 19:26:39 +0900

    [Bronze IV] Title: FA, Time: 64 ms, Memory: 11544 KB -BaekjoonHub

--- 백준/Bronze/14935. FA/FA.java
+++ 백준/Bronze/14935. FA/FA.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String n = br.readLine();
         bw.write("FA");
         bw.flush();
     }
