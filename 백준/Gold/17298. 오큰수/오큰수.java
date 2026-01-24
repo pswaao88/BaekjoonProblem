4d8532a787c79add99971900cf8dee05a8cfb8c9
park sang woo <66404498+pswaao88@users.noreply.github.com>
2026-01-25 01:15:11 +0900

    [Gold IV] Title: 오큰수, Time: 940 ms, Memory: 181976 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -2,49 +2,37 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
-        int[] number = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = N-1; i >= 0; i--){
-            number[i] = Integer.parseInt(st.nextToken());
-        }
-        // 초기화
-        Stack<Integer> stackNumber = new Stack<>();
-        Stack<Integer> stackNGE = new Stack<>();
-        stackNumber.push(number[0]);
-        stackNGE.push(-1);
-
-        for(int i = 1; i < N; i++){
-            // top >= a_i 인 상황
-            if(!stackNumber.isEmpty() && stackNumber.peek() > number[i]){
-                stackNGE.push(stackNumber.peek());
-            }else{
-                while(!stackNumber.isEmpty()){
-                    if(stackNumber.peek() > number[i]){
-                        break;
-                    }
-                    stackNumber.pop();
-                }
-                if(stackNumber.isEmpty()){
-                    stackNGE.push(-1);
-                }else{
-                    stackNGE.push(stackNumber.peek());
-                }
-
-            }
-            stackNumber.push(number[i]);
-        }
-        StringBuilder sb = new StringBuilder();
+        int[] A = new int[N];
         for(int i = 0; i < N; i++){
-            sb.append(stackNGE.pop());
-            sb.append(" ");
+            A[i] = Integer.parseInt(st.nextToken());
         }
-        bw.write(sb.toString().trim());
+
+        StringBuilder sb = new StringBuilder();
+        Deque<Integer> ohNumber = new LinkedList<Integer>();
+        Deque<Integer> printNumber = new LinkedList<>();
+        ohNumber.addFirst(A[N-1]);
+        printNumber.addFirst(-1);
+        for(int i = N-2; i >= 0; i--){
+            while(!ohNumber.isEmpty() && A[i] >= ohNumber.getFirst()){
+                ohNumber.removeFirst();
+            }
+            if(ohNumber.isEmpty()){
+                printNumber.addFirst(-1);
+            }else{
+                printNumber.addFirst(ohNumber.getFirst());
+            }
+            ohNumber.addFirst(A[i]);
+        }
+        for(int x : printNumber){
+            sb.append(x).append(" ");
+        }
+        bw.write(sb.toString());
         bw.flush();
-        bw.close();
-        br.close();
+
     }
 }
