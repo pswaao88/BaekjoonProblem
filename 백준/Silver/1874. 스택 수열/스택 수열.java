3017a59125c9bb69bec3d3d76cff026280f345a0
park sang woo <66404498+pswaao88@users.noreply.github.com>
2026-01-24 17:53:06 +0900

    [Silver II] Title: 스택 수열, Time: 324 ms, Memory: 30484 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -2,36 +2,30 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args)throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
         Stack<Integer> stack = new Stack<>();
-
         StringBuilder sb = new StringBuilder();
         boolean yes = true;
-        int nextNumber = 1;
-        for(int i = 0; i < N; i++){
-            int now = Integer.parseInt(br.readLine());
-            // 불가능한 조건
-            if(!stack.isEmpty() && stack.peek() > now){
+        int now = 1;
+        for(int i = 0; i < n; i++){
+            int stackNumber = Integer.parseInt(br.readLine());
+            if(stack.isEmpty() || stack.peek() < stackNumber){
+                while(now <= stackNumber){
+                    stack.push(now++);
+                    sb.append("+").append("\n");
+                }
+                stack.pop();
+                sb.append("-").append("\n");
+            }else if(stack.peek() == stackNumber){
+                stack.pop();
+                sb.append("-").append("\n");
+            }else{
                 yes = false;
                 break;
             }
-            // push
-            if(stack.isEmpty() || stack.peek() < now){
-                while(nextNumber <= now){
-                    stack.push(nextNumber);
-                    sb.append("+");
-                    sb.append("\n");
-                    nextNumber++;
-                }
-            }
-            // pop
-            stack.pop();
-            sb.append("-");
-            if(i == N-1) continue;
-            sb.append("\n");
         }
         if(yes){
             bw.write(sb.toString());
@@ -39,7 +33,5 @@ public class Main {
             bw.write("NO");
         }
         bw.flush();
-        bw.close();
-        br.close();
     }
 }
