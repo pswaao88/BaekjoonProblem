74a97bd9bfa0c6e5b3467803edacaa3abecd2ecd
park sang woo <66404498+pswaao88@users.noreply.github.com>
2026-01-27 11:44:20 +0900

    [Silver IV] Title: ATM, Time: 108 ms, Memory: 14416 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -8,39 +8,21 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] atm = new int[N];
-        for(int i = 0; i < N; i++){
-            atm[i] = Integer.parseInt(st.nextToken());
+        int[] array = new int[N];
+        int[] sum = new int[N];
+        for(int i = 0; i < N; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
         }
-        // 삽입 정렬
-        int boundary = 1;
-        for(int i = 0; i < N; i++){
-            if(N == 1) break;
-            int now = atm[boundary];
-            int index = boundary;
-            for(int j = 0; j < boundary; j++){
-                if(now < atm[j]){
-                    index = j;
-                    break;
-                }
-            }
-            // 한칸씩 뒤로 밀기
-            for(int j = boundary; j > index; j--){
-                atm[j] = atm[j-1];
-            }
-            atm[index] = now;
-            if(boundary == N-1) break;
-            boundary++;
+        Arrays.sort(array);
+        sum[0] = array[0];
+        int result = sum[0];
+        for(int i = 1; i < N; i++){
+            sum[i] = array[i] + sum[i-1];
+            result += sum[i];
         }
-        int result = 0;
-        int sum = 0;
-        for(int i = 0; i < N; i++){
-            sum += atm[i];
-            result += sum;
-        }
+
         bw.write(Integer.toString(result));
         bw.flush();
-        bw.close();
-        br.close();
     }
+
 }
