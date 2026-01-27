42d88df4593ff43c92be67c4b896fc91f7ddecc1
park sang woo <66404498+pswaao88@users.noreply.github.com>
2026-01-27 11:00:58 +0900

    [Silver III] Title: 주유소, Time: 288 ms, Memory: 30632 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] load = new int[N-1];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N-1; i++){
+            load[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] cost = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            cost[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int min = Integer.MAX_VALUE;
+        long result = 0;
+        for(int i = 0; i < N-1; i++){
+            min = Math.min(min, cost[i]);
+            result += min * load[i];
+        }
+        bw.write(Long.toString(result));
+        bw.flush();
+    }
+}
