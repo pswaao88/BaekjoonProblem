ae53685cabd86512b08d48f170fc82b351f43f3c
park sang woo <66404498+pswaao88@users.noreply.github.com>
2026-01-29 23:41:38 +0900

    [Silver III] Title: 계단 오르기, Time: 108 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,31 +1,22 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int N = Integer.parseInt(br.readLine());
-
         int[] stair = new int[N+1];
         int[] DP = new int[N+1];
-
         for(int i = 1; i <= N; i++){
             stair[i] = Integer.parseInt(br.readLine());
         }
         DP[1] = stair[1];
-        for(int i = 2; i <= N; i++){
-            if(i >= 3){
-                DP[i] = DP[i-2] + stair[i];
-                DP[i] = Math.max(DP[i], stair[i] + stair[i-1] + DP[i-3]);
-            }else{
-                DP[i] = DP[i-1] + stair[2];
-            }
+        if(N >= 2) DP[2] = DP[1] + stair[2];
+        for(int i = 3; i <= N; i++){
+            DP[i] = Math.max(stair[i-1] + DP[i-3], DP[i-2]) + stair[i];
         }
-
         bw.write(Integer.toString(DP[N]));
         bw.flush();
-        bw.close();
-        br.close();
     }
 }
