f89711825375e99b88afb8d6737c8f4064e6852a
park sang woo <66404498+pswaao88@users.noreply.github.com>
2026-01-30 12:57:00 +0900

    [Gold IV] Title: 전화번호 목록, Time: 584 ms, Memory: 44912 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -6,33 +6,36 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int T = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        for(int t = 0; t < T; t++){
-            int n = Integer.parseInt(br.readLine());
-            String[] sortedNumber = new String[n];
-            for(int i = 0; i < n; i++){
-                sortedNumber[i] = br.readLine();
-            }
-            Arrays.sort(sortedNumber);
 
+        StringBuilder sb = new StringBuilder();
+        while(T-->0){
+            int n = Integer.parseInt(br.readLine());
+            String[] number = new String[n];
+            for(int i = 0; i < n; i++){
+                number[i] = br.readLine();
+            }
+            Arrays.sort(number);
             boolean yes = true;
-            for(int i = 0; i < n-1; i++){
-                String now = sortedNumber[i];
-                String next = sortedNumber[i+1];
-                if(now.length() > next.length()) continue;
-                String prefix = next.substring(0, now.length());
-                if(prefix.equals(now)){
+            for(int i = 1; i < n; i++){
+                char[] number1 = number[i-1].toCharArray();
+                char[] number2 = number[i].toCharArray();
+                int count = 0;
+                for(int j = 0; j < number1.length; j++){
+                    if(number1[j] != number2[j]) break;
+                    count++;
+                }
+                if(count == number1.length){
                     yes = false;
                     break;
                 }
-
             }
-            if(yes) sb.append("YES").append("\n");
-            else sb.append("NO").append("\n");
+            if(yes){
+                sb.append("YES\n");
+            }else{
+                sb.append("NO\n");
+            }
         }
         bw.write(sb.toString());
         bw.flush();
-        bw.close();
-        br.close();
     }
 }
