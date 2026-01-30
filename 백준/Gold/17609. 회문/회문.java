948f4f28ca33564516a2d237372336d942a5de79
park sang woo <66404498+pswaao88@users.noreply.github.com>
2026-01-30 12:32:33 +0900

    [Gold V] Title: 회문, Time: 292 ms, Memory: 31584 KB -BaekjoonHub

--- 백준/Gold/17609. 회문/회문.java
+++ 백준/Gold/17609. 회문/회문.java
@@ -2,64 +2,47 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        while(T-- > 0){
-            String now = br.readLine();
-            if(palindrome(0, now.length()-1, now)){
-                sb.append(0);
-            }else if(pseudoPalindrome(0, now.length() - 1, now, 0)){
-                sb.append(1);
-            }else{
-                sb.append(2);
-            }
-            sb.append("\n");
+        while(T-->0){
+            String s = br.readLine();
+            sb.append(isPalindrome(s)).append("\n");
         }
         bw.write(sb.toString());
         bw.flush();
-        bw.close();
-        br.close();
     }
-    static boolean palindrome(int start, int end, String s){
-        char[] now = s.toCharArray();
-        int left = start;
-        int right = end;
-        while(left <= right){
-            if(now[left] != now[right]) return false;
-            left++;
-            right--;
-        }
-        return true;
-    }
-    static boolean pseudoPalindrome(int start, int end, String s, int depth){
-        if(depth > 1){
-            return false;
-        }
-        char[] now = s.toCharArray();
+    static int isPalindrome(String target){
+        for(int i = 0; i < (target.length() + 1) / 2; i++){
+            int left = i;
+            int right = (target.length() - 1) - i;
+            if(target.charAt(left) != target.charAt(right)){
+                String leftTarget = target.substring(left + 1, right + 1);
+                int leftPalindrome = isPseudoPalindrome(leftTarget);
 
-        int left = start;
-        int right = end;
+                String rightTarget = target.substring(left, right);
+                int rightPalindrome = isPseudoPalindrome(rightTarget);
 
-        while(left <= right){
-            if(now[left] != now[right] ) {
-                if(depth != 0){
-                    return false;
+                if(leftPalindrome == 1 || rightPalindrome == 1){
+                    return 1;
+                }else{
+                    return 2;
                 }
-                boolean yes;
-                yes = pseudoPalindrome(left + 1, right, s, depth + 1);
-                if(!yes){
-                    yes = pseudoPalindrome(left, right - 1, s, depth + 1);
-                }
-                return yes;
+
             }
-            left++;
-            right--;
         }
-        return true;
+        return 0;
     }
-
-
+    static int isPseudoPalindrome(String target){
+        for(int i = 0; i < (target.length() + 1) / 2; i++){
+            int left = i;
+            int right = (target.length() - 1) - i;
+            if(target.charAt(left) != target.charAt(right)){
+                return 2;
+            }
+        }
+        return 1;
+    }
 }
