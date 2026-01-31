7af9908b8353cbdc9bdf4820ab22f163d9e37ac1
park sang woo <66404498+pswaao88@users.noreply.github.com>
2026-02-01 02:55:24 +0900

    [Gold V] Title: 평범한 배낭, Time: 136 ms, Memory: 14984 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -8,34 +8,28 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        Product[] products = new Product[N+1];
+        Product_2[] products = new Product_2[N+1];
         for(int i = 1; i <= N; i++){
             st = new StringTokenizer(br.readLine());
-            products[i] = new Product(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            products[i] = new Product_2(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
         }
 
-        int[][] DP = new int[N+1][K+1];
+        int[] DP = new int[K+1];
         for(int i = 1; i <= N; i++){
-            Product now = products[i];
-            int nowW = now.W;
-            int nowV = now.V;
-            for(int j = 1; j <= K; j++){
-                if(nowW <= j){
-                    DP[i][j] = Math.max(DP[i-1][j], DP[i-1][j-nowW] + nowV);
-                }else{
-                    DP[i][j] = DP[i-1][j];
-                }
-
+            Product_2 now = products[i];
+            for(int j = K; j >= 1; j--){
+                if(now.W > j) continue;
+                DP[j] = Math.max(DP[j], DP[j-now.W] + now.V);
             }
         }
-        bw.write(Integer.toString(DP[N][K]));
+        bw.write(Integer.toString(DP[K]));
         bw.flush();
     }
 }
-class Product{
+class Product_2{
     int W;
     int V;
-    Product(int W, int V){
+    Product_2(int W, int V){
         this.W = W;
         this.V = V;
     }
