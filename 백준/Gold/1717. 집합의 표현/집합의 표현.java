0040a887c5a15d73e787e74528e879e7cb8f3a57
park sang woo <66404498+pswaao88@users.noreply.github.com>
2026-02-03 11:28:23 +0900

    [Gold V] Title: 집합의 표현, Time: 424 ms, Memory: 59188 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -3,15 +3,13 @@ import java.util.*;
 
 public class Main {
     static int[] parent;
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st. nextToken());
-        int m = Integer.parseInt(st. nextToken());
-
-        // 기본 초기화
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
         parent = new int[n+1];
         for(int i = 0; i <= n; i++){
             parent[i] = i;
@@ -19,54 +17,39 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < m; i++){
             st = new StringTokenizer(br.readLine());
+            int op = Integer.parseInt(st.nextToken());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            if(a == 0){ // 합집합
-                if(b == c){ // 같으면 그냥 끝
-                    continue;
-                }
 
-                if(b > c){ // 순서 지키기 위해서 경로압축시에 작은거로 합치기 위함
-                    int tmp = b;
-                    b = c;
-                    c = tmp;
-                }
-                union(b, c);
-            }else{ // 같은 집합인지 확인
-
-                if(b == c){
+            if(op == 0){
+                union(a, b);
+            }else{
+                if(sameUnion(a, b)){
                     sb.append("YES\n");
                 }else{
-                    int parentB = find(b);
-                    int parentC = find(c);
-                    if(parentB == parentC){
-                        sb.append("YES\n");
-                    }else{
-                        sb.append("NO\n");
-                    }
+                    sb.append("NO\n");
                 }
             }
         }
         bw.write(sb.toString());
         bw.flush();
-        bw.close();
-        br.close();
 
     }
-    static void union(int a, int b){
-        int parentA = find(a); // a의 부모확인
-        int parentB = find(b); // b의 부모확인
-        // 같은 집합이면 어차피 같음
-        // 다른 집합이라면 작은 쪽으로 합쳐서 합집합 형성
-        parent[parentA] = parentB;
+    static boolean sameUnion(int left, int right){
+        return getParent(left) == getParent(right);
     }
+    static int getParent(int now){
+        if(now == parent[now]) return now;
+        return parent[now] = getParent(parent[now]);
+    }
+    static void union(int left, int right){
+        int leftRoot = getParent(left);
+        int rightRoot = getParent(right);
 
-    static int find(int x){
-        if(parent[x] == x){
-            return x;
+        if(leftRoot < rightRoot){
+            parent[rightRoot] = leftRoot;
+            return;
         }
-        return parent[x] = find(parent[x]);
+        parent[leftRoot] = rightRoot;
     }
-
 }
