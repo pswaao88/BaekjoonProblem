0252e2a071138bbc87cdb08db67fdd7f7e76e6ae
park sang woo <66404498+pswaao88@users.noreply.github.com>
2026-02-04 11:06:21 +0900

    [Silver I] Title: 트리 순회, Time: 108 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -1,67 +1,71 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
-class Node{
-	char alpha;
-	Node left;
-	Node right;
-	public Node(char alpha) {
-		this.alpha = alpha;
-		this.left = null;
-		this.right = null;
-	}
-}
-public class Main {
-	
-	public static void preOrder(Node node) {
-		if(node == null) return;
-		System.out.print(node.alpha);
-		preOrder(node.left);
-		preOrder(node.right);
-	}
-	public static void inOrder(Node node) {
-		if(node == null) return;
-		inOrder(node.left);
-		System.out.print(node.alpha);
-		inOrder(node.right);
-	}
-	public static void postOrder(Node node) {
-		if(node == null) return;
-		postOrder(node.left);
-		postOrder(node.right);
-		System.out.print(node.alpha);
-	}
-	public static void main(String[] args)throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int N = Integer.parseInt(br.readLine());
-		Node[] tree = new Node[26];
-		for(int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			char root = st.nextToken().charAt(0);
-			char left = st.nextToken().charAt(0);
-			char right = st.nextToken().charAt(0);
-			if(tree[root-'A'] == null) {
-				tree[root-'A'] = new Node(root);
-			}
-			if(left != '.') {
-				tree[left-'A'] = new Node(left);
-				tree[root-'A'].left = tree[left-'A'];
-			}
-			if(right != '.') {
-				tree[right-'A'] = new Node(right);
-				tree[root-'A'].right = tree[right-'A'];
-			}
-		}
-		preOrder(tree[0]);
-		System.out.println();
-		inOrder(tree[0]);
-		System.out.println();
-		postOrder(tree[0]);
-		System.out.println();
-	}
+import java.io.*;
+import java.util.*;
 
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static Node[] tree;
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        tree = new Node[26];
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            char a = st.nextToken().charAt(0);
+            char b = st.nextToken().charAt(0);
+            char c = st.nextToken().charAt(0);
+            if(tree[a - 'A'] == null){
+                tree[a - 'A'] = new Node(a, null, null);
+            }
+            if(b != '.'){
+                tree[b - 'A'] = new Node(b, null, null);
+                tree[a - 'A'].left = tree[b - 'A'];
+            }
+            if(c != '.'){
+                tree[c - 'A'] = new Node(c, null, null);
+                tree[a - 'A'].right = tree[c - 'A'];
+            }
+        }
+        preOrder(tree[0]);
+        sb.append("\n");
+        inorder(tree[0]);
+        sb.append("\n");
+        postorder(tree[0]);
+        sb.append("\n");
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+    static void preOrder(Node node){
+        if(node == null) return;
+        sb.append(node.now);
+        preOrder(node.left);
+        preOrder(node.right);
+
+    }
+    static void inorder(Node node){
+        if(node == null) return;
+        inorder(node.left);
+        sb.append(node.now);
+        inorder(node.right);
+
+
+    }
+    static void postorder(Node node){
+        if(node == null) return;
+        postorder(node.left);
+        postorder(node.right);
+        sb.append(node.now);
+    }
+}
+class Node{
+    char now;
+    Node left;
+    Node right;
+    Node(char now, Node left, Node right){
+        this.now = now;
+        this.left = left;
+        this.right = right;
+    }
 }
