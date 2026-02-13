699a2f0ca9b31535c93addd38b5d4f0a81326419
park sang woo <66404498+pswaao88@users.noreply.github.com>
2026-02-13 13:14:24 +0900

    [Gold II] Title: 트리, Time: 372 ms, Memory: 41736 KB -BaekjoonHub

--- 백준/Gold/4256. 트리/트리.java
+++ 백준/Gold/4256. 트리/트리.java
@@ -2,28 +2,33 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n;
-    static Node root;
+    static int n, postIndex;
+    static HashMap<Integer, Integer> rootIndex = new HashMap<>();
     static StringBuilder sb = new StringBuilder();
+    static int[] preOrder, inOrder;
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int T = Integer.parseInt(br.readLine());
         while(T-->0){
-            n = Integer.parseInt(br.readLine());
             StringTokenizer st;
-            int[] preOrder = new int[n];
+            n = Integer.parseInt(br.readLine());
+            postIndex = 0;
+            
+            preOrder = new int[n];
             st = new StringTokenizer(br.readLine());
             for(int i = 0; i < n; i++){
                 preOrder[i] = Integer.parseInt(st.nextToken());
             }
-            int[] inOrder = new int[n];
+
+            inOrder = new int[n];
             st = new StringTokenizer(br.readLine());
             for(int i = 0; i < n; i++){
                 inOrder[i] = Integer.parseInt(st.nextToken());
+                rootIndex.put(inOrder[i], i);
             }
-            findTree(preOrder, inOrder);
-            postOrder(root);
+
+            findPost(0, n - 1);
             sb.append("\n");
         }
         bw.write(sb.toString());
@@ -32,78 +37,15 @@ public class Main {
     // preOrder 순서 루트/왼쪽/오른쪽
     // inOrder 순서 왼쪽/루트/오른쪽
     // preOrder 에서의 루트를 가지고 inOrder에서 왼쪽 오른쪽으로 나누기 반복
-    static Node findTree(int[] preOrder, int[] inOrder){
-        if(preOrder.length == 0){
-            return null;
-        }
+    static void findPost(int start, int end){
+        if(start > end) return;
 
-        if(preOrder.length == 1){
-            return new Node(null, null, preOrder[0]);
-        }
+        int nowRoot = preOrder[postIndex++];
+        int nowRootIndex = rootIndex.get(nowRoot);
+        findPost(start, nowRootIndex - 1);
+        findPost(nowRootIndex + 1, end);
 
-        Node now = new Node(null, null, preOrder[0]);
-        if(preOrder.length == n){
-            root = now;
-        }
-
-        int rootValue = preOrder[0];
-        int rootIndex = 0;
-        for(int i = 0; i < inOrder.length; i++){
-            if(rootValue == inOrder[i]){
-                rootIndex = i;
-                break;
-            }
-        }
-        int leftSize = rootIndex;
-        int rightSize = preOrder.length - rootIndex - 1;
-
-        int[] leftPreOrder = new int[leftSize];
-        int[] leftInOrder = new int[leftSize];
-
-        int[] rightPreOrder = new int[rightSize];
-        int[] rightInOrder = new int[rightSize];
-
-        int leftPreOrderIndex = 0;
-        int leftInOrderIndex = 0;
-        for(int i = 1; i <= leftSize; i++, leftPreOrderIndex++){
-            leftPreOrder[leftPreOrderIndex] = preOrder[i];
-        }
-
-        for(int i = 0; i < rootIndex; i++, leftInOrderIndex++){
-            leftInOrder[leftInOrderIndex] = inOrder[i];
-        }
-
-        int rightPreOrderIndex = 0;
-        int rightInOrderIndex = 0;
-        for(int i = leftSize + 1; i < preOrder.length; i++, rightPreOrderIndex++){
-            rightPreOrder[rightPreOrderIndex] = preOrder[i];
-        }
-
-        for(int i = rootIndex + 1; i < inOrder.length; i++, rightInOrderIndex++){
-            rightInOrder[rightInOrderIndex] = inOrder[i];
-        }
-
-        now.left = findTree(leftPreOrder, leftInOrder);
-        now.right = findTree(rightPreOrder, rightInOrder);
-        return now;
-    }
-    static void postOrder(Node now){
-        if(now.left != null){
-            postOrder(now.left);
-        }
-        if(now.right != null){
-            postOrder(now.right);
-        }
-        sb.append(now.value).append(" ");
+        sb.append(nowRoot).append(" ");
     }
 }
-class Node{
-    Node left;
-    Node right;
-    int value;
-    Node(Node left, Node right, int value){
-        this.left = left;
-        this.right = right;
-        this.value = value;
-    }
-}
+
