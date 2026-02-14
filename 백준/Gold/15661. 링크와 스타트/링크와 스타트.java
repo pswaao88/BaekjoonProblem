fa89764facd36433db50dd38684b642a98fdd4a5
park sang woo <66404498+pswaao88@users.noreply.github.com>
2026-02-14 21:33:20 +0900

    [Gold V] Title: 링크와 스타트, Time: 312 ms, Memory: 21032 KB -BaekjoonHub

--- 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
+++ 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
@@ -6,8 +6,9 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
-        int count = (1 << N);
+        int count = (1 << N) / 2;
         int[][] score = new int[N][N];
+        int[][] sumScore = new int[N][N];
         StringTokenizer st;
         for(int i = 0; i < N; i++){
             st = new StringTokenizer(br.readLine());
@@ -15,43 +16,55 @@ public class Main {
                 score[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        
+
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                sumScore[i][j] = score[i][j] + score[j][i];
+            }
+        }
+
+        int[] teamA = new int[N];
+        int[] teamB = new int[N];
+
         int min = Integer.MAX_VALUE;
         for(int i = 1; i < count; i++){
             // 0이면 팀 A 1이면 팀 B
-            ArrayList<Integer> teamA = new ArrayList<>();
-            ArrayList<Integer> teamB = new ArrayList<>();
             int teamAScore = 0;
             int teamBScore = 0;
+            int teamAIndex = 0;
+            int teamBIndex = 0;
             for(int j = 0; j < N; j++){
                 // 팀 B인경우
                 if((i & (1 << j)) != 0){
-                    teamB.add(j);
+                    teamB[teamBIndex++] = j;
                 }else{
-                    teamA.add(j);
+                    teamA[teamAIndex++] = j;
                 }
             }
-            if(teamA.size() > 1){
-                for(int first = 0; first < teamA.size(); first++){
-                    for(int second = first + 1; second < teamA.size(); second++){
-                        int firstMember = teamA.get(first);
-                        int secondMember = teamA.get(second);
+            if(teamAIndex > 1){
+                for(int first = 0; first < teamAIndex; first++){
+                    for(int second = first + 1; second < teamAIndex; second++){
+                        int firstMember = teamA[first];
+                        int secondMember = teamA[second];
                         teamAScore += score[firstMember][secondMember];
                         teamAScore += score[secondMember][firstMember];
                     }
                 }
             }
-            if(teamB.size() > 1){
-                for(int first = 0; first < teamB.size(); first++){
-                    for(int second = first + 1; second < teamB.size(); second++){
-                        int firstMember = teamB.get(first);
-                        int secondMember = teamB.get(second);
+            if(teamBIndex > 1){
+                for(int first = 0; first < teamBIndex; first++){
+                    for(int second = first + 1; second < teamBIndex; second++){
+                        int firstMember = teamB[first];
+                        int secondMember = teamB[second];
                         teamBScore += score[firstMember][secondMember];
                         teamBScore += score[secondMember][firstMember];
                     }
                 }
             }
             min = Math.min(min, Math.abs(teamAScore - teamBScore));
+            if(min == 0){
+                break;
+            }
         }
         bw.write(Integer.toString(min));
         bw.flush();
