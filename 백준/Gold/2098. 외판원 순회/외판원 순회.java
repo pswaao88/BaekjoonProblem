01aea07d26b3bdb5fc82979c5106fb3383a74091
park sang woo <66404498+pswaao88@users.noreply.github.com>
2026-02-15 15:14:11 +0900

    [Gold I] Title: 외판원 순회, Time: 152 ms, Memory: 19856 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.java
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.java
@@ -2,50 +2,49 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int[][] DP, W;
+    static int N;
+    static final int MAX = 987654321;
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
-        int[][] W = new int[N][N];
+        N = Integer.parseInt(br.readLine());
+        W = new int[N][N];
         StringTokenizer st;
-        for(int i = 0; i < N; i++){
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < N; j++){
+            for (int j = 0; j < N; j++) {
                 W[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        // 어차피 사이클이기 때문에 0번에서 시작해도 됨
-        int[][] DP = new int[(1 << N)][N];
-        for(int nowPath = 0; nowPath < (1 << N); nowPath++){
-            for(int now = 0; now < N; now++){
-                int nowBinary = (1 << now);
-                // 현재까지 위치 & 현재위치 값이 0이 아니면 올바른 path이므로 계산
-                if((nowPath & nowBinary) != 0){
+        DP = new int[N][(1 << N)];
+        for (int i = 0; i < N; i++) Arrays.fill(DP[i], -1);
+        // 0까지 다온경우를 top-down
 
-                    DP[nowPath][now] = 987654321;
-                    int beforePath = (nowPath & (~nowBinary));
-                    // 시작점 처리 (beforePath가 0이면 시작점 처리)
-                    if(beforePath == 0) {
-                        if(now == 0) DP[nowPath][now] = 0; // 시작점 비용은 0
-                        else DP[nowPath][now] = 987654321;       // 0번 아닌데 beforePath가 0일 순 없음
-                        continue;
-                    }
-                    for(int before = 0; before < N; before++){
-                        int beforeBinary = (1 << before);
-                        if((beforePath & beforeBinary) != 0 && W[before][now] != 0){
-                            DP[nowPath][now] = Math.min(DP[nowPath][now], DP[beforePath][before] + W[before][now]);
-                        }
-                    }
-                }
-            }
-        }
-        int min = 987654321;
-        for(int i = 1; i < N; i++){
-            if(DP[(1 << N) - 1][i] != 0 && W[i][0] != 0){
-                min = Math.min(min, DP[(1 << N) - 1][i] + W[i][0]);
-            }
-        }
+        int min = dfs(0, 1);;
+
         bw.write(Integer.toString(min));
         bw.flush();
+    }
+    static int dfs(int now, int visit){
+        if(visit == (1<<N) - 1){
+            if(W[now][0] != 0) return W[now][0];
+            return MAX;
+        }
+        // 이미 방문한 곳
+        if(DP[now][visit] != -1){
+            return DP[now][visit];
+        }
+        // 방문안했으므로 MAX로 초기화
+        DP[now][visit] = MAX;
+
+        for(int next = 0; next < N; next++){
+            // 길이 있고, 아직 방문하지 않은 도시라면
+            if (W[now][next] != 0 && (visit & (1 << next)) == 0) {
+                int result = dfs(next, visit | (1 << next)) + W[now][next];
+                DP[now][visit] = Math.min(DP[now][visit], result);
+            }
+        }
+        return DP[now][visit];
     }
 }
