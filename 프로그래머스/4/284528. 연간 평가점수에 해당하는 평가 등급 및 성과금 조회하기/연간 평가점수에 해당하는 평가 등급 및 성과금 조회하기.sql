a8ad13dc915cbf0326e9c6c4d0269037cb28ab13
park sang woo <66404498+pswaao88@users.noreply.github.com>
2026-03-05 16:46:42 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,32 +1,33 @@
-WITH TEAR_SUM AS (
-    SELECT 
-        EMP_NO,
-        AVG(SCORE) AS SCORE
-    FROM HR_GRADE GROUP BY EMP_NO
-), TOTAL AS (
+# 사원별 성과금
+# 점수 별로 성과금 비율이 다름
+# 사번, 성명, 평가 등급, 성과금
+
+WITH A AS(
     SELECT 
         he.EMP_NO,
         he.EMP_NAME,
-        CASE
-            WHEN hg.SCORE >= 96 THEN 'S'
-            WHEN hg.SCORE >= 90 THEN 'A'
-            WHEN hg.SCORE >= 80 THEN 'B'
-            ELSE 'C'
-        END AS GRADE,
-        SAL
+        hg.SCORE,
+        he.SAL
     FROM HR_EMPLOYEES he
-    INNER JOIN TEAR_SUM hg
+    INNER JOIN HR_GRADE hg
     ON he.EMP_NO = hg.EMP_NO
 )
+
 SELECT 
     EMP_NO,
     EMP_NAME,
-    GRADE,
     CASE
-        WHEN GRADE = 'S' THEN SAL * 0.2
-        WHEN GRADE = 'A' THEN SAL * 0.15
-        WHEN GRADE = 'B' THEN SAL * 0.1
-        ELSE SAL * 0
+        WHEN 96 <= AVG(SCORE) THEN 'S'
+        WHEN 90 <= AVG(SCORE) THEN 'A'
+        WHEN 80 <= AVG(SCORE) THEN 'B'
+        ELSE 'C'
+    END AS GRADE,
+    CASE
+        WHEN 96 <= AVG(SCORE) THEN SAL * 0.2
+        WHEN 90 <= AVG(SCORE) THEN SAL * 0.15
+        WHEN 80 <= AVG(SCORE) THEN SAL * 0.1
+        ELSE 0
     END AS BONUS
-FROM TOTAL
+FROM A
+GROUP BY EMP_NO, EMP_NAME
 ORDER BY EMP_NO ASC;
