fcf2a9e4751a67394409bbf063a98251364b222e
park sang woo <66404498+pswaao88@users.noreply.github.com>
2026-03-05 15:16:21 +0900

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -1,24 +1,30 @@
-WITH REST AS (
-    SELECT 
-        i.REST_ID,
-        i.REST_NAME, 
-        i.FOOD_TYPE, 
-        i.FAVORITES,   
-        i.ADDRESS, 
-        r.REVIEW_SCORE
-    FROM REST_INFO i
-    INNER JOIN REST_REVIEW r 
-    ON i.REST_ID = r.REST_ID
+# 서울에 위치한 식당
+# 식당 ID, 음식 종류, 즐겨찾기 수, 주소, 리뷰 평균 점수(ROUNT(, 2)
+# 평균점수로 내림 차순, 즐겨찾기 내림차순
+WITH A AS (
+    SELECT  
+        REST_ID,
+        REST_NAME,
+        FOOD_TYPE,
+        FAVORITES,
+        ADDRESS
+    FROM REST_INFO
+    WHERE ADDRESS LIKE '서울%'
+), B AS (
+    SELECT
+        REST_ID,
+        ROUND(AVG(REVIEW_SCORE), 2) AS SCORE
+    FROM REST_REVIEW
+    GROUP BY REST_ID
 )
-
-SELECT 
-    REST_ID, 
-    REST_NAME, 
-    FOOD_TYPE, 
-    FAVORITES, 
-    ADDRESS, 
-    ROUND(AVG(REVIEW_SCORE), 2) AS SCORE
-FROM REST 
-WHERE SUBSTRING(ADDRESS, 1, 2) = '서울'
-GROUP BY REST_ID
-ORDER BY SCORE DESC, FAVORITES DESC;
+SELECT
+    a.REST_ID,
+    a.REST_NAME,
+    a.FOOD_TYPE,
+    a.FAVORITES,
+    a.ADDRESS,
+    b.SCORE
+FROM A a
+INNER JOIN B b
+ON a.REST_ID = b.REST_ID
+ORDER BY b.SCORE DESC, a.FAVORITES DESC;
