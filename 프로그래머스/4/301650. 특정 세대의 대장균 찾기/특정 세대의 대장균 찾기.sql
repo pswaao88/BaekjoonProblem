6eed8a6e3824e7d03f44711ffdeee01a1b50788c
park sang woo <66404498+pswaao88@users.noreply.github.com>
2026-03-05 17:45:12 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,21 +1,20 @@
-WITH RECURSIVE TREE AS (
-    -- 루트 부분으로 1회만 실행
-    SELECT 
-        ID, 
-        PARENT_ID , 
-        1 AS DEPTH 
-    FROM ECOLI_DATA WHERE PARENT_ID IS NULL
-    -- 이 아래부터 재귀 파트
+WITH RECURSIVE A AS(
+    # root
+    SELECT
+        ID,
+        1 AS DEPTH
+    FROM ECOLI_DATA
+    WHERE PARENT_ID IS NULL
+    
     UNION ALL
-    -- root부터 시작해서 재귀 돌리기
-    -- e는 자식 t가 부모의 역할
-    SELECT 
-        e.ID, 
-        e.PARENT_ID, 
-        t.DEPTH + 1
+    
+    SELECT
+        e.ID,
+        a.DEPTH + 1 AS DEPTH
     FROM ECOLI_DATA e
-    INNER JOIN 
-    TREE t ON e.PARENT_ID = t.ID
+    INNER JOIN A a
+    ON e.PARENT_ID = a.ID
 )
-
-SELECT ID FROM TREE WHERE DEPTH = 3;
+SELECT ID FROM A 
+WHERE DEPTH = 3
+ORDER BY ID ASC ;
