953c307f775677ed1ee235b12e6570298f625619
park sang woo <66404498+pswaao88@users.noreply.github.com>
2026-03-27 15:30:20 +0900

    [level 3] Title: 정수 삼각형, Time: 13.56 ms, Memory: 62.5 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/README.md
+++ 프로그래머스/3/43105. 정수 삼각형/README.md
@@ -1,10 +1,10 @@
 # [level 3] 정수 삼각형 - 43105 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105?language=java) 
 
 ### 성능 요약
 
-메모리: 65.1 MB, 시간: 29.86 ms
+메모리: 62.5 MB, 시간: 13.56 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 01일 00:36:31
+2026년 03월 27일 15:30:16
 
 ### 문제 설명
 
--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,23 +1,24 @@
 class Solution {
     public int solution(int[][] triangle) {
-        int answer = 1;
-        int[][] DP = new int[triangle.length][triangle.length];
+        int answer = 0;
+        int N = triangle.length;
+        // 위 또는 왼쪽에서 가져오기
+        int[][] DP = new int[N][N];
         DP[0][0] = triangle[0][0];
-        for(int i = 1; i < triangle.length; i++){
-            for(int j = 0; j <= i; j++){
-                if(j == 0){
-                    DP[i][j] = DP[i-1][j] + triangle[i][j];
-                }else{
-                    DP[i][j] = Math.max(DP[i-1][j], DP[i-1][j-1]);
-                    DP[i][j] += triangle[i][j];   
+        for(int i = 1; i < N; i++){
+            int[] now = triangle[i];
+            for(int j = 0; j < now.length; j++){
+                if(j != 0){
+                    DP[i][j] = DP[i-1][j-1];    
+                }
+                DP[i][j] = Math.max(DP[i][j], DP[i-1][j]);
+                DP[i][j] += now[j];
+                if(i == N-1){
+                    answer = Math.max(answer, DP[i][j]);
                 }
             }
         }
         
-        for(int i = 0; i < triangle.length; i++){
-            System.out.println(DP[triangle.length - 1][i]);
-            answer = Math.max(answer, DP[triangle.length - 1][i]);
-        }
         return answer;
     }
 }
